ca61e694813a5d8c30bf5c8714b767ff153efc30
Fpsska <fpsska1337@gmail.com>
2022-10-13 20:43:35 +0300

    feat: create style.scss, _vars.scss, _reset.scss, _functions.scss files, rigister in index.tsx component

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import './index.css';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
+
+import './index.css';
+import './assets/styles/base/_reset.scss';
+import './assets/styles/base/style.scss';
 
 // /. imports
 
